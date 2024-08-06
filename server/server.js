5c8e60d05ed442017d2d31d9d6dfc6e2e593f306
Romiro <johnson.romiro@yahoo.com>
2024-08-06 11:29:00 +0100

    Made changes to deploy on Render

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+
 const express = require('express');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
@@ -5,6 +8,11 @@ const cakeRoutes = require('./routes/cakeRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path  = require('path');
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+console.log(__dirname);
 
 dotenv.config();
 connectDB();
@@ -16,7 +24,10 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/cakes', cakeRoutes);
 app.use('/api/orders', orderRoutes);
+app.use(express.static(path.join(__dirname, './client/build')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './client/build/index.html')));
 
 
+// app.use(express.static('./client'))
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
